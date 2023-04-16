bf32da9f1d5ed9c9b871604e642b784a35a2362f
Chadi Tawbi <98048498+chaditawbi@users.noreply.github.com>
2023-04-16 23:08:43 +0200

    Update db.js

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const URI = "mongodb+srv://Achraf_Haddar:Achraf123@cluster0.cun4h.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+
 const URI = "mongodb+srv://Jadslim:Jadslim2@cluster2.01rm9tk.mongodb.net/Birdy?retryWrites=true&w=majority"
 
 const connectDB = async() => {
